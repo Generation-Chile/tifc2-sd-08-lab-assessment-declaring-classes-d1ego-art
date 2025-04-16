2ae9d2deb0f47faf3eb6d1b9ff584c39a54a85c3
d1ego-art <145490502+d1ego-art@users.noreply.github.com>
2025-04-16 03:48:42 +0000

    Player- entrega

--- task4.js
+++ task4.js
@@ -1,7 +1,16 @@
 export class Player {
-  constructor() {
-   
-  }
+  constructor(name, level) {
+    this.name = name;
+    this.level = level;
+     
   
+  }
+info() {
+  console.log(`${this.name} has reached Level ${this.level}!`);
+
+}
+levelUp() {
+  this.level = this.level + 1
+} 
   
 }
